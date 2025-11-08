5af8376d6400cbd31fef3040f384470b5fd662b6
akshaygite24 <akshaygite2004@gmail.com>
2025-11-08 22:47:22 +0530

    Added Extract function for data loading from csv

---
+++ etl_pipeline.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import numpy as np
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+DB_URL = "sqlite:///retail_data.db"
+DATA_FILE = "data/Superstore.csv"
+
+def extract():
+    logging.info("Extracting dataset...")
